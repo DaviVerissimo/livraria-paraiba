d0f24159dd38f4e6c93f2fad2615a493c8aa240c
ferrazandre <55859517+ferrazandre@users.noreply.github.com>
2021-05-22 15:39:36 -0300

    commit incial

---
+++ src/main/java/br/edu/ifpb/dac/livrariaParaiba/LivrariaParaibaApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.dac.livrariaParaiba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivrariaParaibaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivrariaParaibaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifpb/dac/livrariaParaiba/LivrariaParaibaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.dac.livrariaParaiba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivrariaParaibaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
