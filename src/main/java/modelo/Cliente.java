29b15ebd164d7c90d2abea81e537326f48dfc824
ferrazandre <55859517+ferrazandre@users.noreply.github.com>
2021-05-24 10:55:35 -0300

    classe autor
    classe autor jpa

---
+++ src/main/java/modelo/Cliente.java
@@ -0,0 +1,114 @@
+package modelo;
+
+import java.io.Serializable;
+import java.sql.Date;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name = "autor")
+public class Cliente implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	private long id;
+	private String nome;
+	
+	@Temporal(TemporalType.DATE)
+	private Date nascimento;
+	private String rua, bairro, cidade, estado;
+	private int numero;
+	@ManyToMany(mappedBy = "autor")
+	private List<Livro> listaLivros;
+	
+	public Cliente() {
+		
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Date getNascimento() {
+		return nascimento;
+	}
+
+	public void setNascimento(Date nascimento) {
+		this.nascimento = nascimento;
+	}
+
+	public String getRua() {
+		return rua;
+	}
+
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public List<Livro> getListaLivros() {
+		return listaLivros;
+	}
+
+	public void setListaLivros(List<Livro> listaLivros) {
+		this.listaLivros = listaLivros;
+	}
+	
+	
+}
--- src/main/java/modelo/Usuario.java
+++ src/main/java/modelo/Usuario.java
@@ -1,5 +1,37 @@
 package modelo;
 
-public class Usuario {
+public abstract class Usuario  {
 
+	private String username, senha;
+	private boolean estado;
+	
+	public abstract boolean verificarLogin(String user, String senha);
+	
+	public abstract boolean isLogado();
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public boolean isEstado() {
+		return estado;
+	}
+
+	public void setEstado(boolean estado) {
+		this.estado = estado;
+	}
+
+	
 }
