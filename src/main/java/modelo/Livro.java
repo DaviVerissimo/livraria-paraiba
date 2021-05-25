4b508ea316bb11a17dc145e87dbed0f0963861bd
bruno <bruno.bzrrasantos@gmail.com>
2021-05-24 22:18:59 -0300

    Update Livro.java
    Alterações iniciais em Livro.java

--- src/main/java/modelo/Livro.java
+++ src/main/java/modelo/Livro.java
@@ -1,5 +1,105 @@
 package modelo;
 
-public class Livro {
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Livro implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8235602197506319410L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	private Integer id;
+
+	private Integer edicao;
+
+	private String genero;
+
+	private String nome;
+
+	private BigDecimal valor;
+
+	private String descricao;
+
+	private String isbn;
+
+	private Integer nPaginas;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Integer getEdicao() {
+		return edicao;
+	}
+
+	public void setEdicao(Integer edicao) {
+		this.edicao = edicao;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public BigDecimal getValor() {
+		return valor;
+	}
+
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public Integer getnPaginas() {
+		return nPaginas;
+	}
+
+	public void setnPaginas(Integer nPaginas) {
+		this.nPaginas = nPaginas;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
 
 }
