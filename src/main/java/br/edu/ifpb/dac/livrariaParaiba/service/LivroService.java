0d487edfa77a7bf0f1e1297217672ac8862dc05c
bruno <bruno.bzrrasantos@gmail.com>
2021-05-26 00:59:17 -0300

    Adição das camadas de serviço sem implementação

--- src/main/java/br/edu/ifpb/dac/livrariaParaiba/service/LivroService.java
+++ src/main/java/br/edu/ifpb/dac/livrariaParaiba/service/LivroService.java
@@ -1,5 +1,16 @@
 package br.edu.ifpb.dac.livrariaParaiba.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.edu.ifpb.dac.livrariaParaiba.modelo.Livro;
+import br.edu.ifpb.dac.livrariaParaiba.repositorio.LivroRepository;
+
+@Service
 public interface LivroService {
+	
+	public void cadastrarLivro(Livro livro);
+	public void editarLivro(Livro aSerEditado);
+	public void removerLivro(Livro removido);
 	
 }
