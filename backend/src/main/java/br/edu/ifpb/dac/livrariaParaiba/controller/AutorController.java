c96b9978bdf3503adfb5ec1d0034791f63710c65
ferrazandre <55859517+ferrazandre@users.noreply.github.com>
2021-07-13 10:04:44 -0300

    teste thymeleaf
    ajustes nos pacotes e dependencias

--- backend/src/main/java/br/edu/ifpb/dac/livrariaParaiba/controller/AutorController.java
+++ backend/src/main/java/br/edu/ifpb/dac/livrariaParaiba/controller/AutorController.java
@@ -1,5 +1,14 @@
 package br.edu.ifpb.dac.livrariaParaiba.controller;
 
-public class AutorController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
+public class AutorController {
+	
+	
+	@GetMapping("/autor")
+	public String autor() {
+		return "autor";
+	}
 }
