dc79fc0fbfb4d041a73a49517510a8b4bbfe7266
ferrazandre <55859517+ferrazandre@users.noreply.github.com>
2021-07-16 20:56:04 -0300

    lista de autores (inicial)

--- backend/src/main/java/br/edu/ifpb/dac/livrariaParaiba/controller/AutorController.java
+++ backend/src/main/java/br/edu/ifpb/dac/livrariaParaiba/controller/AutorController.java
@@ -1,14 +1,29 @@
 package br.edu.ifpb.dac.livrariaParaiba.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import br.edu.ifpb.dac.livrariaParaiba.service.AutorService;
 
 @Controller
 public class AutorController {
 	
 	
+	private AutorService autorService;
+	
+	public AutorController(AutorService autorService) {
+		this.autorService = autorService;
+	}
+	
 	@GetMapping("/autor")
 	public String autor() {
 		return "autor";
+	} 
+	@GetMapping("/autores")
+	public String autores(Model model) {
+		model.addAttribute("listaAutores", autorService.retornarListaDeAutores());
+		return "autor/index";
 	}
 }
