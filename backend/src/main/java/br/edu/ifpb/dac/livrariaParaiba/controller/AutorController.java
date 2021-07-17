d91c43e2dcf084a79477959c0fd0ceffc464d152
ferrazandre <55859517+ferrazandre@users.noreply.github.com>
2021-07-16 22:28:17 -0300

    alterar e excluir autor

--- backend/src/main/java/br/edu/ifpb/dac/livrariaParaiba/controller/AutorController.java
+++ backend/src/main/java/br/edu/ifpb/dac/livrariaParaiba/controller/AutorController.java
@@ -1,10 +1,13 @@
 package br.edu.ifpb.dac.livrariaParaiba.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import br.edu.ifpb.dac.livrariaParaiba.model.Autor;
@@ -18,8 +21,13 @@ public class AutorController {
 
 	@GetMapping("/autores/novo")
 	public String autor(@ModelAttribute("autor") Autor autor) {
-
 		return "autor/form";
+	}
+	
+	@PostMapping("/autor/salvar")
+	public String salvarAutor(@ModelAttribute("autor") Autor autor) {
+		autorService.salvar(autor);
+		return "redirect:/autores";
 	}
 
 	@GetMapping("/autores")
@@ -28,9 +36,24 @@ public class AutorController {
 		return "autor/index";
 	}
 
-	@PostMapping("/autor/salvar")
-	public String salvarAutor(@ModelAttribute("autor") Autor autor) {
-		autorService.salvar(autor);
+	@GetMapping("/autor/{ID}")
+	public String alterarAutor(@PathVariable("ID") long id, Model model) {
+		Optional<Autor> autorOpt = Optional.ofNullable(autorService.pesquisarAutorPorID(id));
+		if (autorOpt.isEmpty()) {
+			throw new IllegalArgumentException("Autor inválido.");
+		}
+		model.addAttribute("autor", autorOpt.get());
+		return "autor/form";
+	}
+	
+	@GetMapping("/autor/excluir/{ID}")
+	public String excluirAutor(@PathVariable ("ID") long id) {
+		Optional<Autor> autorOpt = Optional.ofNullable(autorService.pesquisarAutorPorID(id));
+		if (autorOpt.isEmpty()) {
+			throw new IllegalArgumentException("Autor inválido.");
+		}
+		
+		autorService.remove(autorOpt.get().getID());
 		return "redirect:/autores";
 	}
 }
