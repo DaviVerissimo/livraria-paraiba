efcfb2324f6278b3a56aed66d3c9de88d7674937
ferrazandre <55859517+ferrazandre@users.noreply.github.com>
2021-07-16 21:51:37 -0300

    cadastro de autor

--- backend/src/main/java/br/edu/ifpb/dac/livrariaParaiba/controller/AutorController.java
+++ backend/src/main/java/br/edu/ifpb/dac/livrariaParaiba/controller/AutorController.java
@@ -4,26 +4,33 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import br.edu.ifpb.dac.livrariaParaiba.model.Autor;
 import br.edu.ifpb.dac.livrariaParaiba.service.AutorService;
 
 @Controller
 public class AutorController {
-	
-	
+
+	@Autowired
 	private AutorService autorService;
-	
-	public AutorController(AutorService autorService) {
-		this.autorService = autorService;
+
+	@GetMapping("/autores/novo")
+	public String autor(@ModelAttribute("autor") Autor autor) {
+
+		return "autor/form";
 	}
-	
-	@GetMapping("/autor")
-	public String autor() {
-		return "autor";
-	} 
+
 	@GetMapping("/autores")
 	public String autores(Model model) {
 		model.addAttribute("listaAutores", autorService.retornarListaDeAutores());
 		return "autor/index";
+	}
+
+	@PostMapping("/autor/salvar")
+	public String salvarAutor(@ModelAttribute("autor") Autor autor) {
+		autorService.salvar(autor);
+		return "redirect:/autores";
 	}
 }
